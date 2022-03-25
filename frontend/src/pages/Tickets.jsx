70641212942bd44f1250942035c52c14a1cc8cee
Daniel Kim <danielkim2711@gmail.com>
2022-03-25 14:45:01 +1300

    List Tickets in UI

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
+import TicketItem from '../components/TicketItem';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
@@ -27,7 +28,23 @@ const Tickets = () => {
     return <Spinner />;
   }
 
-  return <div>Tickets</div>;
+  return (
+    <>
+      <BackButton url='/' />
+      <h1>Tickets</h1>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Tickets;
